bd4bb57b90d87ecf0f3d6ea933c24c177cc31817
Jung Hoon Yoon <juhuyoon@yahoo.com>
2018-05-06 21:13:05 -0400

    learning java

---
+++ HelloWorld/src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.example.helloworld;
+/** Learning how to use Java **/
+public class HelloWorld {
+    public static void main(String[] args) { /** can use Cntrl + J to bring down live templates **/
+        System.out.println("Hello, World");
+    }
+}
