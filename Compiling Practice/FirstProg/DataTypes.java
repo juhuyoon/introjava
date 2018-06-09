5b8d96b9278803dea7717178b6582921c1f4b885
Jung Hoon Yoon <juhuyoon@yahoo.com>
2018-06-08 22:24:46 -0400

    java syntax learning

---
+++ Compiling Practice/FirstProg/DataTypes.java
@@ -0,0 +1,26 @@
+int maxInt = 2147483647; //normal integer
+long muchMOre = 214748347 * 10000000; //long = much larger integer. Storage/calculation takes much longer also only store natural numbers. 
+double fraction = 99.275; //takes longer to calculate, used to store large numbers or just fraction numbers. 
+
+String fulltext = 'text of some sentence';
+char answer = 'b';
+char three = '3'; //char = character storage, it also can only store ONE LETTER so a '10' won't be accepted.
+
+boolean fact = true;
+boolean condition = false; //true or false storage
+
+
+//Numbers
+//use double to do accurate math divisions
+double specific=  5/2.0;
+double specific2 = 24/5.0;
+
+//Casting
+double div = 5/2 //results in 2.5, but returns 2 via truncations
+double current = 17;
+double rate = 1.5;
+double future = current * rate;
+System.out.println(future); //output = 25.5
+int approx = (int) future; //output is 25, and this (int) is casting. 
+
+
