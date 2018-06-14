d4852445cf16f99822f920435992e1b16142f026
Jung Hoon Yoon <juhuyoon@yahoo.com>
2018-06-13 21:02:11 -0400

    simples

--- Compiling Practice/FirstProg/ControlFlow.java
+++ Compiling Practice/FirstProg/ControlFlow.java
@@ -79,6 +79,8 @@ System.out.println(coffeeType);
 //random function practices:
 //public = access modifier that allows anyone to use this public functions
 //void = return type. This function is not returning any data. When it's only performing actions and not returning any results, then void is the use. 
+//void = printing is only for our eyes, so it doesn't actually make an output where something is shown. Return value can be anything. 
+//from void, it can be string, int, boolean, etc to specify the return value for the function. 
 //function name = "chorus", just the name. 
 
 //parameters = variables in the parentheses of our function, that we can use inside our function
@@ -109,3 +111,31 @@ public void playMusic() {
 
 //setting variables with int, string etc?
 int something = 15; 
+
+
+
+//changing from void to int to return the output that is desired.
+//returning currentLikes, which is an integer, so the storage return is int
+public int likePhoto(int currentLikes, String comment, boolean like) {
+    System.out.println("Feedback: " + comment);
+    if(like) {
+        currentLikes = currentLikes + 1;
+    }
+    System.out.println("Number of likes: " + currentLikes);
+    return currentLikes;
+
+}
+
+int returnedLikes = likePhoto(0, "nice", true); //returns 1
+int totalLikes = likePhoto(returnedLikes, "I like this", true); //now it's the integer of 1 instead of zero and totalLikes counts up to 2. 
+
+
+// Define a function with the name and parameters: makeChange(double itemCost, double dollarsProvided)
+// Calculate and return the calculated change which should be the dollarsProvided minus the itemCost
+
+// Remember to return a value you need 1) a return type and 2) a return statement
+
+public double makeChange(double itemCost, double dollarsProvided) {
+    double change = dollarsProvided - itemCost;
+    return change;
+}
